46e7b281e3f331eaf8029689e0ffb608d5d1d86e
Soburr <adebesinbenzenejr99@gmail.com>
2024-04-20 08:54:35 +0100

    portals

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PortalController;
+use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/student-dashboard', [PortalController::class, 'index']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/sign-in', [RegistrationController::class, 'showRegistrationForm'])->name('sign-in');
+Route::post('/sign-in', [RegistrationController::class, 'register']);
 
-require __DIR__.'/auth.php';
+Route::get('/login', [RegistrationController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [RegistrationController::class, 'login']);
+
+
+// require __DIR__.'/auth.php';
