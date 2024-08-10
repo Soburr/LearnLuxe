f809084db5020f2b2b9dbd2ab9fb2823b656dd1c
Soburr <adebesinbenzenejr99@gmail.com>
2024-08-10 18:29:15 +0100

    feature/update-bio

--- resources/views/student/update-profile.blade.php
+++ resources/views/student/update-profile.blade.php
@@ -6,10 +6,119 @@
     <title>Document</title>
 </head>
 <body>
-<h2>Update Your Bio-Data</h2>
-{{ $name}}
-{{ $student_id}}
-{{ $email}}
+
+    <style>
+        nav {
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            width: 500px;
+            margin: 0 auto;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+        }
+
+        .input-container {
+            position: relative;
+            width: 300px;
+            margin: 0 auto;
+            text-align: center;
+        }
+
+        .placeholder {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 25%;
+            color: black;
+            font-weight: bold;
+            padding: 20px;
+            background-color: gray;
+            border: 2px solid gray;
+            border-radius: 10px 0px 0px 10px;
+            ;
+            z-index: 1;
+        }
+
+        .input-field {
+            width: 300px;
+            padding: 20px;
+            border: 2px solid gray;
+            border-radius: 10px 25px 20px 10px;
+        }
+
+        .submit-button {
+            margin-top: 10px;
+            padding: 10px 20px;
+            border: none;
+            margin: 10px auto;
+            display: block;
+            border-radius: 5px;
+            background-color: #007bff;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        .submit-button:hover {
+            background-color: #0056b3;
+        }
+
+        .input-field:focus+.placeholder {
+            display: none;
+        }
+    </style>
+
+    <nav>
+        <h2 style="text-align: center;">Update Your Bio-Data</h2>
+        <hr><br>
+        <div class="default">
+            <p
+                style="border: 2px solid gray;
+                  padding: 20px;
+                  margin: 0px auto;
+                  width: 300px;
+                  background-color: gray;
+                  font-weight: bold;
+                  border-radius: 10px 25px 20px 10px;">
+                {{ $name }}</p> <br>
+            <p
+                style="border: 2px solid gray;
+                  padding: 20px;
+                  margin: 0 auto;
+                  width: 300px;
+                  background-color: gray;
+                  font-weight: bold;
+                  border-radius: 10px 25px 20px 10px;">
+                {{ $student_id }}</p> <br>
+            <p
+                style="border: 2px solid gray;
+                  padding: 20px;
+                  margin: 0 auto;
+                  width: 300px;
+                  background-color: gray;
+                  font-weight: bold;
+                  border-radius: 10px 25px 20px 10px;">
+                {{ $email }}</p> <br>
+        </div>
+
+        <div class="input-container">
+            <span class="placeholder">D.O.B</span>
+            <input type="text" class="input-field"> <br><br>
+        </div>
+
+        <div class="input-container">
+            <span class="placeholder">CLASS</span>
+            <input type="text" class="input-field"> <br><br>
+        </div>
+
+        <div class="input-container">
+            <span class="placeholder">GENDER</span>
+            <input type="text" class="input-field"> <br><br>
+        </div>
+        <hr>
+        <button class="submit-button">Submit</button>
+    </nav>
+
+
 
 </body>
 </html>
